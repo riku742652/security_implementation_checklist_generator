64fdbf512a9daed9ceaf956376dc751be8fed85f
riku742652 <riku742652@gmail.com>
2023-03-18 13:08:58 +0900

    fixup! add: error handling

--- generator.go
+++ generator.go
@@ -31,28 +31,48 @@ func main() {
 		question_cell := "G" + strconv.Itoa(rows_num)
 		answer_cell := "E" + strconv.Itoa(rows_num)
 		question, err := file.GetCellValue(sheet_name, question_cell)
-		if question != "" && strings.Index(question, "※") == -1 {
+		if question != "" && strings.Index(question, "※") == -1 && strings.Index(question, "。") != -1 {
 			answer, err := file.GetCellValue(sheet_name, answer_cell)
+			// 複数項目のケア
+			// すでに対応済みの場合はスキップ
+			if strings.Index(answer, "■ 対応済") != -1 {
+				rows_num++
+				continue
+			}
+			if strings.Index(answer, "■ 未対策") != -1 {
+				answer = strings.Replace(answer, "■ 未対策", "□ 未対策", -1)
+			}
+			if strings.Index(answer, "■ 対応不要") != -1 {
+				answer = strings.Replace(answer, "■ 対応不要", "□ 対応不要", -1)
+			}
 			fmt.Println(question)
-			// fmt.Println(answer)
 			answer = strings.Replace(answer, "※\n", "", -1)
 			answer_list := strings.Split(answer, "\n")
 
 			for num, ans := range answer_list {
-				fmt.Println(num+1, ans)
+				fmt.Println(num+1, strings.Replace(ans, "□", "", -1))
 			}
-			// fmt.Println(ans)
 			if err = rows.Close(); err != nil {
 				fmt.Println(err)
 			}
 			var str string
-			fmt.Scan(&str)
-			number, _ := strconv.Atoi(str)
-			answer = strings.Replace(answer, "□", "■", number)
-			file.SetCellValue(sheet_name, answer_cell, answer)
+			for {
+				fmt.Print("Answer: ")
+				fmt.Scan(&str)
+				number, err := strconv.Atoi(str)
 
+				if err != nil || number > len(answer_list) {
+					fmt.Println("Error: 値が不正です")
+				} else {
+					test_str := strings.Replace(answer_list[number-1], "□", "■", -1)
+					answer = strings.Replace(answer, answer_list[number-1], test_str, -1)
+					file.SetCellValue(sheet_name, answer_cell, answer)
+					break
+				}
+			}
 		}
 		rows_num++
 	}
 	file.SaveAs("output.xlsx")
+	fmt.Println("Output Completed.")
 }
